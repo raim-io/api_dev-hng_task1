bf0aa9d62a945b0a493b228b627f554a50e8fe91
raim-io <raim.tobi@gmail.com>
2023-09-08 09:35:08 +0100

    implemented motionjs for generating current date and time within json response

--- server.js
+++ server.js
@@ -1,29 +1,52 @@
 const express = require('express');
+const moment = require('moment');
 
 const PORT = 8080;
 
+// initialize express app
 const app = express();
 
+// define the current date and current time
+const date = moment();
+const currentDay = date.format('dddd');
+const utcTime = date.toISOString();
+
+//const date = new Date();
+//const currentDay = date.toLocaleDateString('en-US', { weekday: 'long' });
+//const utcTime = date.toISOString();
+
+// test api server
 app.get('/', (req, res) => {
 	res.send('API is running...');
 });
 
-app.listen(PORT, console.log(`server is running on port ${PORT}`));
-
-const date = new Date();
-const currentDay = date.toLocaleDateString('en-US', { weekday: 'long' });
-const utcTime = date.toISOString();
-
+// main api endpoint
 app.get('/api/zuri', (req, res) => {
 	const { slack_name, track } = req.query;
 
-	const result = {
-		slack_name,
-		current_day: currentDay,
-		utc_time: utcTime,
-		track,
-		github_file_url,
-		github_repo_url,
-		status_code
-	};
+	try {
+		if (!slack_name || !track) {
+			console.log('Input your slack_name and track');
+
+			return res.sendStatus(400);
+		}
+
+		const result = {
+			slack_name,
+			current_day: currentDay,
+			utc_time: utcTime,
+			track,
+			"github_file_url": "",
+			"github_repo_url": "",
+			status_code: "200"
+		};
+
+		res.json(result);
+	} catch (error) {
+		res.status(400);
+		throw new Error(error.message);
+	}
 });
+
+
+app.listen(PORT, console.log(`server is running on port ${PORT}`));
